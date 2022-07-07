7ccd052c18d6ec6f0dc67508aa4a0c8c657eaf09
kk-skillimprovements <108892682+kk-skillimprovements@users.noreply.github.com>
2022-07-07 17:03:57 -0400

    Update streamlit_app.py
    added more breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@
 import streamlit
 
 streamlit.title('May Parents New Healthy Dinner')
+
+
+streamlit.title('BreakFast Menu')
+
+streamlit.title('Omega 3 & blueberry oatmeal')
+streamlit.title('Kale , Spinach & Rocket smoothie')
+
