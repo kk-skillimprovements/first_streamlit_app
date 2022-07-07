5ef22d8b3690bfa86e542a1f9c777510da6103d7
kk-skillimprovements <108892682+kk-skillimprovements@users.noreply.github.com>
2022-07-07 17:09:07 -0400

    Update streamlit_app.py
    add emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ streamlit.title('May Parents New Healthy Dinner')
 
 streamlit.header('BreakFast Menu')
 
-streamlit.text('Omega 3 & blueberry oatmeal')
-streamlit.text('Kale , Spinach & Rocket smoothie')
+streamlit.text(' 🥣 Omega 3 & blueberry oatmeal')
+streamlit.text(' 🥗 Kale , Spinach & Rocket smoothie')
+streamlit.text(' 🥑🍞 Avacado Toast')
 
